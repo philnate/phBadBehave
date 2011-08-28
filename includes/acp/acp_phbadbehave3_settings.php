6b7e69b2bc40cbd1eee3afd9e41518939f63b720
philnate <philnate@gmail.com>
2011-08-28 15:11:43 +0200

    setting storage done

--- includes/acp/acp_phbadbehave3_settings.php
+++ includes/acp/acp_phbadbehave3_settings.php
@@ -5,12 +5,29 @@ class acp_phbadbehave3_settings
    var $new_config;
    function main($id, $mode)
    {
-      global $db, $user, $auth, $template;
-      global $config, $phpbb_root_path, $phpbb_admin_path, $phpEx;
-	$this->page_title = 'ACP_PBB3_TITLE_SETTINGS';
-	$this->tpl_name = 'acp_phbadbehave3_settings';
+		global $db, $user, $auth, $template;
+		global $config, $phpbb_root_path, $phpbb_admin_path, $phpEx;
+		$this->page_title = 'ACP_PBB3_TITLE_SETTINGS';
+		$this->tpl_name = 'acp_phbadbehave3_settings';
 
-	//allow switching bad behavior version
+		if (false !== request_var('submit', false)) {
+			//page was submitted check if all values are as expected;
+
+			set_config('pbb3_log', 	(request_var('pbb3_log', true))? 'true' : 'false');
+			set_config('pbb3_verbose', (request_var('pbb3_verbose', false))? 'true' : 'false');
+			set_config('pbb3_strict', (request_var('pbb3_strict', false))? 'true' : 'false');
+			set_config('pbb3_offsite', (request_var('pbb3_offsite', false))? 'true' : 'false');
+			set_config('pbb3_httpbl_maxage', (int) request_var('pbb3_httpbl_maxage', 30));
+			set_config('pbb3_httpbl_level', (int) request_var('pbb3_httpbl_level', 25));
+			set_config('pbb3_httpbl_key', preg_replace('#[^a-z]#', '', strtolower(request_var('pbb3_httpbl_key', ''))));
+		}
+
+		$result = $db->sql_query('SELECT * FROM ' . CONFIG_TABLE . ' WHERE `config_name` LIKE \'' . $db->sql_escape('pbb3_%') . '\'');
+		$rows =	$db->sql_fetchrowset($result);
+		foreach ($rows as $row) {
+			$template->assign_var('S_' . strtoupper($row['config_name']), ('true' == $row['config_value'])? 'checked' : $row['config_value']);
+		}
+		//TODO allow switching bad behavior version
 
    }
 }
